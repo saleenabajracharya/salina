bc12d7f482464d6af6b28a1e6d8e49d8f8b12155
saleenabajracharya <saleena.bajracharya12@gmail.com>
2020-02-29 11:12:45 +0545

    salina

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// $a=5;
+// $b=4;
+// echo "<h1>Hello ACHS $a $b</h1> <br>";
+// echo '<h1>Hello ACHS $a </h1>';
+// //array
+// $cars=array("volvo","Kia","maruti","Tesla");
+// echo "Diff cars are 1.".$cars[0]."<br>2.".$cars[1];
+// echo strlen("Hello world!");
+// echo str_word_count("Hello world!"); 
+// echo strrev("Hello world!"); 
+// $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+}
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $value) {
+  echo "$value <br>";
+}
+
+
+?>
